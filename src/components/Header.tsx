be5906e438c8eb6d72de65df12358b9853c099e8
naotaka nakane <n.nakane0219@gmail.com>
2020-04-19 09:11:35 +0900

    ignorecase

---
+++ src/components/Header.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+
+type IProps = {
+  siteTitle: string
+}
+
+const Header: React.SFC<IProps> = ({ siteTitle }) => (
+  <_Header>
+    <_H1>
+      <_Link to="/">{siteTitle}</_Link>
+    </_H1>
+  </_Header>
+)
+
+const _Header = styled.header`
+  box-sizing: border-box;
+  height: 40px;
+  padding: 0 16px;
+  display: flex;
+  align-items: center;
+  background-color: #0f4c81;
+  color: #ffffff;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.26);
+`
+
+const _Link = styled(Link)`
+  box-sizing: border-box;
+  text-decoration: none;
+  &:visited {
+    color: inherit;
+  }
+`
+
+const _H1 = styled.h1`
+  box-sizing: border-box;
+  font-size: 24px;
+  margin: 0;
+`
+
+export default Header
